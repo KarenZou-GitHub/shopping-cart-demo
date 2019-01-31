9bab325838346b4315c081f51c33290b11009acf
fisher-monkey <980937033@qq.com>
2019-01-31 14:52:15 +0800

    chore: 在组件中注册子组件，建立元素名称与引入子组件对象的关系

--- src/components/Demo/TestModule.vue
+++ src/components/Demo/TestModule.vue
@@ -5,10 +5,10 @@
       <h1>例子一. 子->父的$emit $on方法</h1>
       <hr/>
       <p>总数：{{ total }}</p>
-      <myComponentTestEmit 
+      <my-component-test-emit 
         @increase="handleGetTotal"
         @reduce="handleGetTotal"
-      ></myComponentTestEmit>
+      ></my-component-test-emit >
 <!-- 这个地方，把组件注册那里的emit函数改成特殊的this.$emit('input',this.counter),然后把这个组件的两个函数换成v-model="total"也可以实时获得total 。是个语法糖-->
     </div>
 <!-- 以上，例一--> 
@@ -29,7 +29,7 @@
       <h1>例子三. 高级组件之内联模板</h1>
       <span>组件的第三种注册方法</span>
       <hr/>
-      <learn-module-no3-inline-template inline-template>
+      <learn-module-no3-inline-template :message="message" inline-template>
         <div>
           <h3>在父组件定义子组件的模板</h3>
           <!-- 目前上面这一句不显示,好奇怪啊 -->
@@ -46,12 +46,28 @@
 <script>
   import myComponentTestEmit from './my-component-testemit'
   import Vue from 'vue'
+
+ var learnModuleNo3InlineTemplate={
+    name: 'learn-module-no3-inline-template',
+    data: function(){
+      return {
+        msg:'在子组件data中声明的数据123'
+        //message: '在父组件data中声明的数据12'
+      }
+    }
+  }
+
   export default {
     data(){
       return{
         total:0,
         sum:100,
-        message: '在父组件data中声明的数据11'
+        message: '在父组件data中声明的数据1'
+      }
+    },
+    props(){
+      return {
+         message: '在父组件data中声明的数据1'
       }
     },
     methods:{
@@ -63,17 +79,11 @@
       }
     },
     components:{
-      myComponentTestEmit
+      "my-component-test-emit ":myComponentTestEmit,
+      "learn-module-no3-inline-template":learnModuleNo3InlineTemplate
     }
   }
-  Vue.component('learn-module-no3-inline-template',{
-    data: function(){
-      return {
-        msg:'在子组件data中声明的数据'
-        //message: '在父组件data中声明的数据12'
-      }
-    }
-  })
+ 
 </script>
 <style>
   .div{
